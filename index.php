561221fddcf818074ea006086bbf3be8620226d3
nicomanunta <nic.manun@gmail.com>
2024-01-31 16:50:40 +0100

    stampati a schermo i cibi

--- index.php
+++ index.php
@@ -8,10 +8,10 @@
         }
     }
 
-    $cane = new Categoria("Cane");
-    $gatto = new Categoria("Gatto");
-    $uccelli = new Categoria("Uccelli");
-    $pesci = new Categoria("Pesci");
+    $cane = new Categoria("<i class='fa-solid fa-dog' style='color: black;'></i>");
+    $gatto = new Categoria("<i class='fa-solid fa-cat' style='color: black;'></i>");
+    $uccelli = new Categoria("<i class='fa-solid fa-crow' style='color: black;'></i>");
+    $pesci = new Categoria("<i class='fa-solid fa-fish' style='color: black;'></i>");
 
     // classe PRODOTTO
     class Prodotto{
@@ -88,15 +88,16 @@
     
     
     
-    $gioco_1 = new Gioco("Kong Classic", 6.90, "<img src='https://arcaplanet.vtexassets.com/arquivos/ids/256599/kong-classic1.jpg' alt=''>", $cane, "Plastica");
-    $gioco_2 = new Gioco("Topino Trixie", 10.90, "<img src='https://arcaplanet.vtexassets.com/arquivos/ids/223852/trixie-gatto-gioco-active-mouse-peluche.jpg' alt=''>", $gatto, "Peluche");
+    $gioco_1 = new Gioco("Kong Classic", "6.90", "<img src='https://arcaplanet.vtexassets.com/arquivos/ids/256599/kong-classic1.jpg' alt=''>", $cane, "Plastica");
+    $gioco_2 = new Gioco("Topino Trixie", "10.90", "<img src='https://arcaplanet.vtexassets.com/arquivos/ids/223852/trixie-gatto-gioco-active-mouse-peluche.jpg' alt=''>", $gatto, "Peluche");
     
-    $cibo_1 = new Cibo("Royal Canin Mini Adult", 15.90, "<img src='https://arcaplanet.vtexassets.com/arquivos/ids/284621/Mini-Adult.jpg?v=638182891693570000' alt=''>", $cane, "Secco");
-    $cibo_2 = new Cibo("Almo Nature Holistic Maintenance Medium Large Tonno e Riso", 12.90, "<img src='https://arcaplanet.vtexassets.com/arquivos/ids/245173/almo-nature-holistic-cane-adult-medium-pollo-e-riso.jpg' alt=''>", $cane, "Secco");
-    $cibo_3 = new Cibo("Almo Nature Cat Daily Lattina", 16.90, "<img src='https://arcaplanet.vtexassets.com/arquivos/ids/245336/almo-daily-menu-cat-400-gr-vitello.jpg' alt=''>", $pesci, "Secco");
+    $cibo_1 = new Cibo("Royal Canin Mini Adult", "15.90", "<img src='https://arcaplanet.vtexassets.com/arquivos/ids/284621/Mini-Adult.jpg?v=638182891693570000' alt=''>", $cane, "Secco");
+    $cibo_2 = new Cibo("Almo Nature Holistic Maintenance Medium Large Tonno e Riso", "12.90", "<img src='https://arcaplanet.vtexassets.com/arquivos/ids/245173/almo-nature-holistic-cane-adult-medium-pollo-e-riso.jpg' alt=''>", $cane, "Secco");
+    $cibo_3 = new Cibo("Almo Nature Cat Daily Lattina", "16.90", "<img src='https://arcaplanet.vtexassets.com/arquivos/ids/245336/almo-daily-menu-cat-400-gr-vitello.jpg' alt=''>", $gatto, "Liquido");
+    $cibo_4 = new Cibo("Mangime per Pesci Guppy in Fiocchi", "8.90", "<img src='https://arcaplanet.vtexassets.com/arquivos/ids/272714/tetra-guppy-mini-flakes.jpg' alt=''>", $pesci, "Secco");
     
-    $accessorio_1 = new Accessorio("Voliera Wilma in Legno", 60, "<img src='https://arcaplanet.vtexassets.com/arquivos/ids/258384/voliera-wilma1.jpg' alt=''>", $uccelli,  "100-60-50cm");
-    $accessorio_2 = new Accessorio("Cartucce Filtranti per Filtro EasyCrystal", 19.90, "<img src='https://arcaplanet.vtexassets.com/arquivos/ids/272741/tetra-easycrystal-filterpack-250-300.jpg' alt=''>", $pesci, "Per vasche max: 60l");
+    $accessorio_1 = new Accessorio("Voliera Wilma in Legno", "60", "<img src='https://arcaplanet.vtexassets.com/arquivos/ids/258384/voliera-wilma1.jpg' alt=''>", $uccelli,  "100-60-50cm");
+    $accessorio_2 = new Accessorio("Cartucce Filtranti per Filtro EasyCrystal", "19.90", "<img src='https://arcaplanet.vtexassets.com/arquivos/ids/272741/tetra-easycrystal-filterpack-250-300.jpg' alt=''>", $pesci, "Per vasche max: 60l");
     ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -105,27 +106,62 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
 </head>
 <body>
     <!-- <img src='' alt=''> -->
     <?php
-    echo $gioco_1-> getInfo();
-    echo $gioco_2-> getInfo();
-    echo $cibo_1-> getInfo();
-    echo $cibo_2-> getInfo();
-    echo $cibo_3-> getInfo();
-    echo $accessorio_1-> getInfo();
-    echo $accessorio_2-> getInfo();
+    // echo $gioco_1-> getInfo();
+    // echo $gioco_2-> getInfo();
+    // echo $cibo_1-> getInfo();
+    // echo $cibo_2-> getInfo();
+    // echo $cibo_3-> getInfo();
+    // echo $accessorio_1-> getInfo();
+    // echo $accessorio_2-> getInfo();
     ?>
-    <div class="container">
+    <div class="container mt-5">
         <div class="row">
-            <div class="col-12">
+            <h1>Cibo</h1>
+            <div class="col-3">
                 <div class="card" style="width: 18rem;">
-                    <?php echo ?>
+                    <?php echo $cibo_1-> getImg();?>
                     <div class="card-body">
-                        <h5 class="card-title">Card title</h5>
-                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                        <h5 class="card-title"> <?php echo $cibo_1-> getNome(); ?></h5>
+                        <p class="card-text"><?php echo "Tipo: ".$cibo_1-> getTipo()." ".$cibo_1-> getCategoria(); ?></p>
+                        <p class="card-text"><?php echo "Prezzo: ".$cibo_1->getPrezzo()."€"; ?></p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-3">
+                <div class="card" style="width: 18rem;">
+                    <?php echo $cibo_2-> getImg();?>
+                    <div class="card-body">
+                        <h5 class="card-title"> <?php echo $cibo_2-> getNome(); ?></h5>
+                        <p class="card-text"><?php echo "Tipo: ".$cibo_2-> getTipo()." ".$cibo_2-> getCategoria(); ?></p>
+                        <p class="card-text"><?php echo "Prezzo: ".$cibo_2->getPrezzo()."€"; ?></p>
+               
+                    </div>
+                </div>
+            </div>
+            <div class="col-3">
+                <div class="card" style="width: 18rem;">
+                    <?php echo $cibo_3-> getImg();?>
+                    <div class="card-body">
+                        <h5 class="card-title"> <?php echo $cibo_3-> getNome(); ?></h5>
+                        <p class="card-text"><?php echo "Tipo: ".$cibo_3-> getTipo()." ".$cibo_3-> getCategoria(); ?></p>
+                        <p class="card-text"><?php echo "Prezzo: ".$cibo_3->getPrezzo()."€"; ?></p>
+               
+                    </div>
+                </div>
+            </div>
+            <div class="col-3">
+                <div class="card" style="width: 18rem;">
+                    <?php echo $cibo_4-> getImg();?>
+                    <div class="card-body">
+                        <h5 class="card-title"> <?php echo $cibo_4-> getNome(); ?></h5>
+                        <p class="card-text"><?php echo "Tipo: ".$cibo_4-> getTipo()." ".$cibo_4-> getCategoria(); ?></p>
+                        <p class="card-text"><?php echo "Prezzo: ".$cibo_4->getPrezzo()."€"; ?></p>
+               
                     </div>
                 </div>
             </div>
