3a5f8ab9e19e7874215609aa5c661f6fc15d0cc3
nicomanunta <nic.manun@gmail.com>
2024-01-31 17:03:54 +0100

    stampate tutte le card

--- index.php
+++ index.php
@@ -97,7 +97,7 @@
     $cibo_4 = new Cibo("Mangime per Pesci Guppy in Fiocchi", "8.90", "<img src='https://arcaplanet.vtexassets.com/arquivos/ids/272714/tetra-guppy-mini-flakes.jpg' alt=''>", $pesci, "Secco");
     
     $accessorio_1 = new Accessorio("Voliera Wilma in Legno", "60", "<img src='https://arcaplanet.vtexassets.com/arquivos/ids/258384/voliera-wilma1.jpg' alt=''>", $uccelli,  "100-60-50cm");
-    $accessorio_2 = new Accessorio("Cartucce Filtranti per Filtro EasyCrystal", "19.90", "<img src='https://arcaplanet.vtexassets.com/arquivos/ids/272741/tetra-easycrystal-filterpack-250-300.jpg' alt=''>", $pesci, "Per vasche max: 60l");
+    $accessorio_2 = new Accessorio("Cartucce Filtranti per Filtro EasyCrystal", "19.90", "<img src='https://arcaplanet.vtexassets.com/arquivos/ids/272741/tetra-easycrystal-filterpack-250-300.jpg' alt=''>", $pesci, " vasche max 60l");
     ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -107,18 +107,9 @@
     <title>Document</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
+    <link rel="stylesheet" href="./css/style.css">
 </head>
 <body>
-    <!-- <img src='' alt=''> -->
-    <?php
-    // echo $gioco_1-> getInfo();
-    // echo $gioco_2-> getInfo();
-    // echo $cibo_1-> getInfo();
-    // echo $cibo_2-> getInfo();
-    // echo $cibo_3-> getInfo();
-    // echo $accessorio_1-> getInfo();
-    // echo $accessorio_2-> getInfo();
-    ?>
     <div class="container mt-5">
         <div class="row">
             <h1>Cibo</h1>
@@ -126,9 +117,9 @@
                 <div class="card" style="width: 18rem;">
                     <?php echo $cibo_1-> getImg();?>
                     <div class="card-body">
-                        <h5 class="card-title"> <?php echo $cibo_1-> getNome(); ?></h5>
-                        <p class="card-text"><?php echo "Tipo: ".$cibo_1-> getTipo()." ".$cibo_1-> getCategoria(); ?></p>
-                        <p class="card-text"><?php echo "Prezzo: ".$cibo_1->getPrezzo()."€"; ?></p>
+                        <h5 class="card-title text-center mb-4"> <?php echo $cibo_1-> getNome(); ?></h5>
+                        <p class="card-text text-center"><?php echo "Tipo: ".$cibo_1-> getTipo()." ".$cibo_1-> getCategoria(); ?></p>
+                        <p class="card-text text-center"><?php echo "Prezzo: ".$cibo_1->getPrezzo()."€"; ?></p>
                     </div>
                 </div>
             </div>
@@ -136,9 +127,9 @@
                 <div class="card" style="width: 18rem;">
                     <?php echo $cibo_2-> getImg();?>
                     <div class="card-body">
-                        <h5 class="card-title"> <?php echo $cibo_2-> getNome(); ?></h5>
-                        <p class="card-text"><?php echo "Tipo: ".$cibo_2-> getTipo()." ".$cibo_2-> getCategoria(); ?></p>
-                        <p class="card-text"><?php echo "Prezzo: ".$cibo_2->getPrezzo()."€"; ?></p>
+                        <h5 class="card-title text-center mb-4"> <?php echo $cibo_2-> getNome(); ?></h5>
+                        <p class="card-text text-center"><?php echo "Tipo: ".$cibo_2-> getTipo()." ".$cibo_2-> getCategoria(); ?></p>
+                        <p class="card-text text-center"><?php echo "Prezzo: ".$cibo_2->getPrezzo()."€"; ?></p>
                
                     </div>
                 </div>
@@ -147,9 +138,9 @@
                 <div class="card" style="width: 18rem;">
                     <?php echo $cibo_3-> getImg();?>
                     <div class="card-body">
-                        <h5 class="card-title"> <?php echo $cibo_3-> getNome(); ?></h5>
-                        <p class="card-text"><?php echo "Tipo: ".$cibo_3-> getTipo()." ".$cibo_3-> getCategoria(); ?></p>
-                        <p class="card-text"><?php echo "Prezzo: ".$cibo_3->getPrezzo()."€"; ?></p>
+                        <h5 class="card-title text-center mb-4"> <?php echo $cibo_3-> getNome(); ?></h5>
+                        <p class="card-text text-center"><?php echo "Tipo: ".$cibo_3-> getTipo()." ".$cibo_3-> getCategoria(); ?></p>
+                        <p class="card-text text-center"><?php echo "Prezzo: ".$cibo_3->getPrezzo()."€"; ?></p>
                
                     </div>
                 </div>
@@ -158,10 +149,60 @@
                 <div class="card" style="width: 18rem;">
                     <?php echo $cibo_4-> getImg();?>
                     <div class="card-body">
-                        <h5 class="card-title"> <?php echo $cibo_4-> getNome(); ?></h5>
-                        <p class="card-text"><?php echo "Tipo: ".$cibo_4-> getTipo()." ".$cibo_4-> getCategoria(); ?></p>
-                        <p class="card-text"><?php echo "Prezzo: ".$cibo_4->getPrezzo()."€"; ?></p>
+                        <h5 class="card-title text-center mb-4"> <?php echo $cibo_4-> getNome(); ?></h5>
+                        <p class="card-text text-center"><?php echo "Tipo: ".$cibo_4-> getTipo()." ".$cibo_4-> getCategoria(); ?></p>
+                        <p class="card-text text-center"><?php echo "Prezzo: ".$cibo_4->getPrezzo()."€"; ?></p>
                
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="container mt-5">
+        <div class="row">
+            <h1>Giochi</h1>
+            <div class="col-3">
+                <div class="card" style="width: 18rem;">
+                    <?php echo $gioco_1-> getImg();?>
+                    <div class="card-body">
+                        <h5 class="card-title text-center mb-4"> <?php echo $gioco_1-> getNome(); ?></h5>
+                        <p class="card-text text-center"><?php echo "Materiale: ".$gioco_1-> getMateriale()." ".$gioco_1-> getCategoria(); ?></p>
+                        <p class="card-text text-center"><?php echo "Prezzo: ".$gioco_1->getPrezzo()."€"; ?></p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-3">
+                <div class="card" style="width: 18rem;">
+                    <?php echo $gioco_2-> getImg();?>
+                    <div class="card-body">
+                        <h5 class="card-title text-center mb-4"> <?php echo $gioco_2-> getNome(); ?></h5>
+                        <p class="card-text text-center"><?php echo "Materiale: ".$gioco_2-> getMateriale()." ".$gioco_2-> getCategoria(); ?></p>
+                        <p class="card-text text-center"><?php echo "Prezzo: ".$gioco_2->getPrezzo()."€"; ?></p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="container my-5">
+        <div class="row">
+            <h1>Accessori</h1>
+            <div class="col-3">
+                <div class="card" style="width: 18rem;">
+                    <?php echo $accessorio_1-> getImg();?>
+                    <div class="card-body">
+                        <h5 class="card-title text-center mb-4"> <?php echo $accessorio_1-> getNome(); ?></h5>
+                        <p class="card-text text-center"><?php echo "Dimensioni: ".$accessorio_1-> getDimensioni()." ".$accessorio_1-> getCategoria(); ?></p>
+                        <p class="card-text text-center"><?php echo "Prezzo: ".$accessorio_1->getPrezzo()."€"; ?></p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-3">
+                <div class="card" style="width: 18rem;">
+                    <?php echo $accessorio_2-> getImg();?>
+                    <div class="card-body">
+                        <h5 class="card-title text-center mb-4"> <?php echo $accessorio_2-> getNome(); ?></h5>
+                        <p class="card-text text-center"><?php echo "Dimensioni: ".$accessorio_2-> getDimensioni()." ".$accessorio_2-> getCategoria(); ?></p>
+                        <p class="card-text text-center"><?php echo "Prezzo: ".$accessorio_2->getPrezzo()."€"; ?></p>
                     </div>
                 </div>
             </div>
